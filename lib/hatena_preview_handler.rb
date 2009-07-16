07b7ecbbc3d95796139e44a43fdd9d37a7593074
koseki <koseki@gmail.com>
2009-07-17 02:54:41 +0900

    separate

---
+++ lib/hatena_preview_handler.rb
@@ -0,0 +1,29 @@
+class HatenaPreviewHandler < WEBrick::HTTPServlet::DefaultFileHandler
+  include ERB::Util
+
+  TEMPLATE = ERB.new(File.read(File.dirname(__FILE__) + "/hatena_preview_server.erb"), nil, "%-")
+
+  def initialize(server, path)
+    super
+    @local_path = path
+  end
+
+  def do_GET(req,res)
+    if @local_path !~ %r{/(\d{4}-\d{2}-\d{2})(?:-.+)?\.txt$}
+      super
+      return
+    end
+    date = Time.parse($1)
+    
+    src = File.read(@local_path)
+    (title, body) = src.split(/\r\n|\r|\n/, 2)
+
+    parser = Text::Hatena.new
+    parser.parse(body)
+    body = parser.html
+
+    res['Content-Type'] = "text/html"
+    res.body = TEMPLATE.result(binding)
+  end
+end
+
